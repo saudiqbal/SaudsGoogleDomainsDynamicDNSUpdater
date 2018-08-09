6a79b396be313de942f18c125d785f54f623b338
Admin <Admin@DESKTOP>
2018-08-09 01:12:12 -0400

    Initial Commit

---
+++ SaudsGoogleDomainsDynamicDNSUpdater/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace SaudsGoogleDomainsDynamicDNSUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.subdomain = new System.Windows.Forms.TextBox();
+            this.interval = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 240);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(655, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(77, 17);
+            this.toolStripStatusLabel2.Text = "Status: Ready";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Sub Domain";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 164);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Check Interval";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(132, 65);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(240, 20);
+            this.username.TabIndex = 5;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(132, 98);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(240, 20);
+            this.password.TabIndex = 6;
+            // 
+            // subdomain
+            // 
+            this.subdomain.Location = new System.Drawing.Point(132, 129);
+            this.subdomain.Name = "subdomain";
+            this.subdomain.Size = new System.Drawing.Size(240, 20);
+            this.subdomain.TabIndex = 7;
+            // 
+            // interval
+            // 
+            this.interval.Location = new System.Drawing.Point(132, 161);
+            this.interval.Name = "interval";
+            this.interval.Size = new System.Drawing.Size(118, 20);
+            this.interval.TabIndex = 8;
+            this.interval.Text = "60";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(323, 207);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(133, 13);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "http://www.saudiqbal.com";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(270, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "in minutes";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Maximize";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 262);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.interval);
+            this.Controls.Add(this.subdomain);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Saud\'s Google Domains Dynamic DNS Updater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox subdomain;
+        private System.Windows.Forms.TextBox interval;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ SaudsGoogleDomainsDynamicDNSUpdater/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Diagnostics;
+
+namespace SaudsGoogleDomainsDynamicDNSUpdater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MaximizeBox = false;
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+            this.SizeGripStyle = SizeGripStyle.Hide;
+            // When window state changed, trigger state update.
+            this.Resize += SetMinimizeState;
+
+            // When tray icon clicked, trigger window state change.       
+            notifyIcon1.Click += ToggleMinimizeState;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (username.Text == "")
+            {
+                MessageBox.Show("Please enter the username");
+                username.Focus();
+                return;
+            }
+            if (password.Text == "")
+            {
+                MessageBox.Show("Please enter the password");
+                password.Focus();
+                return;
+            }
+            if (subdomain.Text == "")
+            {
+                MessageBox.Show("Please enter the subdomain");
+                subdomain.Focus();
+                return;
+            }
+            if (interval.Text == "")
+            {
+                MessageBox.Show("Please enter the interval in minutes");
+                interval.Focus();
+                return;
+            }
+            int val = 0;
+            bool res = Int32.TryParse(interval.Text, out val);
+            if (res == true && val > 0 && val < 35792)
+            {
+                // add record
+            }
+            else
+            {
+                MessageBox.Show("Please input 1 to 35791 only.");
+                interval.Focus();
+                return;
+            }
+
+            try
+            {
+                var client = new WebClient { Credentials = new NetworkCredential(username.Text, password.Text) };
+                var response = client.DownloadString("https://domains.google.com/nic/update?hostname=" + subdomain.Text);
+                //MessageBox.Show(response);
+                toolStripStatusLabel2.Text = "Status: " + response + DateTime.Now.ToString(" - yyyy-MM-dd h:mm tt");
+                //responseddns.Content = response;
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ReceiveFailure || ex.Status == WebExceptionStatus.ConnectFailure || ex.Status == WebExceptionStatus.KeepAliveFailure)
+                {
+                    toolStripStatusLabel2.Text = "Status: Connection Failed" + DateTime.Now.ToString(" - yyyy-MM-dd h:mm tt");
+                }
+            }
+
+            Properties.Settings.Default.usernamesave = username.Text;
+            Properties.Settings.Default.passwordsave = password.Text;
+            Properties.Settings.Default.subdomainsave = subdomain.Text;
+            Properties.Settings.Default.intervalsave = interval.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            username.Text = Properties.Settings.Default.usernamesave;
+            password.Text = Properties.Settings.Default.passwordsave;
+            subdomain.Text = Properties.Settings.Default.subdomainsave;
+            interval.Text = Properties.Settings.Default.intervalsave;
+
+
+            System.Windows.Forms.Timer MyTimer = new System.Windows.Forms.Timer();
+            int intervaldigit = Convert.ToInt32(interval.Text);
+            MyTimer.Interval = (intervaldigit * 60000); // 45 mins
+            MyTimer.Tick += new EventHandler(MyTimer_Tick);
+            MyTimer.Start();
+        }
+
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                var client = new WebClient { Credentials = new NetworkCredential(username.Text, password.Text) };
+                var response = client.DownloadString("https://domains.google.com/nic/update?hostname=" + subdomain.Text);
+                //MessageBox.Show(response);
+                toolStripStatusLabel2.Text = "Status: " + response + DateTime.Now.ToString(" - yyyy-MM-dd h:mm tt");
+                //responseddns.Content = response;
+            }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ReceiveFailure || ex.Status == WebExceptionStatus.ConnectFailure || ex.Status == WebExceptionStatus.KeepAliveFailure)
+                {
+                    toolStripStatusLabel2.Text = "Status: Connection Failed" + DateTime.Now.ToString(" - yyyy-MM-dd h:mm tt");
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            ProcessStartInfo sInfo = new ProcessStartInfo("http://www.saudiqbal.com");
+            Process.Start(sInfo);
+        }
+
+        // Toggle state between Normal and Minimized.
+        private void ToggleMinimizeState(object sender, EventArgs e)
+        {
+            bool isMinimized = this.WindowState == FormWindowState.Minimized;
+            this.WindowState = (isMinimized) ? FormWindowState.Normal : FormWindowState.Minimized;
+        }
+
+        // Show/Hide window and tray icon to match window state.
+        private void SetMinimizeState(object sender, EventArgs e)
+        {
+            bool isMinimized = this.WindowState == FormWindowState.Minimized;
+
+            this.ShowInTaskbar = !isMinimized;
+            notifyIcon1.Visible = isMinimized;
+            //if (isMinimized) notifyIcon1.ShowBalloonTip(100, "Saud's Google Domains Dynamic DNS Updater", "Application minimized to tray.", ToolTipIcon.Info);
+        }
+    }
+}
